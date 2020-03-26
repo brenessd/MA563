08f530aba6303e3dcb8fc1ff333a54ac8802c467
Stacy Brenes <brenes.sbc@gmail.com>
2020-03-26 17:29:49 -0400

    Chore. Created and added the cobweb diagrams for each function in problem 2

--- Homework 1/Problem 2/graphFunction.m
+++ Homework 1/Problem 2/graphFunction.m
@@ -1,19 +1,20 @@
+
 function y=graphFunction(x)
   
 % A. Logistic Equation
-%     y = 3.5*x.*(1-x);
+    y = 3.2*x.*(1-x);
 
 % B. Composed Logistic Equation
 %     f= 3.5*x.*(1-x);
 %     y= 3.5*f.*(1-f);
-% 
+
 % C. Tent Map
-    if x < 1/2 
-        y= 2*x;
-    else 
-        y=2*(1-x);
-    end
-        
+%     if x < 1/2 
+%         y= 2*x;
+%     else 
+%         y=2*(1-x);
+%     end
+%         
 
 % D. Circle Map
 %     y =mod(3*x, 1);  
